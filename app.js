21d39fe9f78e73a0156d4d15654c98c7d90a046e
imrankhan76 <ik7080367@gmail.com>
2024-10-29 12:44:01 +0500

    Random Password generator website clone
    A simple Random Password Generator web application built with HTML, CSS, Bootstrap, and JavaScript. It allows users to create customizable, secure passwords with various character options and copy them

---
+++ app.js
@@ -0,0 +1,10 @@
+function GenerateRandomPass() {
+    const length = 25; // You can change the length of the password
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("passwordInput").value = password; // Set the generated password to the input
+}
